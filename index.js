8861423e950c7344392b929fee6657a5e8358540
Hirabayashi623 <ryo662233@gmail.com>
2018-06-09 01:40:31 +0900

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env['heroku.webapp.port'] || 5000;
 // アプリ生成
 var app = express();
 
