f8cc02cc1227bf1a2aa9f6066b2725cde7933da4
Hirabayashi623 <ryo662233@gmail.com>
2018-06-09 01:39:03 +0900

    commit

--- index.js
+++ index.js
@@ -11,7 +11,3 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
