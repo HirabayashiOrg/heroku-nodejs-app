f396bc6a080e926df785850b119ea31e892796c5
Hirabayashi623 <ryo662233@gmail.com>
2018-08-07 20:06:28 +0900

    2018/08/07_20:06:27.35

--- public/js/todo_react/list.ts
+++ public/js/todo_react/list.ts
@@ -1,14 +1,35 @@
 (() => {
-	const tasks = [
-		{id: 0, task: 'task1', status: 1},
-		{id: 1, task: 'task2', status: 1},
-		{id: 2, task: 'task3', status: 0},
+	var tasks = [
+		{id: 0, task: 'task1', active: true},
+		{id: 1, task: 'task2', active: true},
+		{id: 2, task: 'task3', active: false},
 	];
+
+	function Task(props) {
+		const task = props.task;
+
+		return (
+			<li className={props.task.active?'list-group-item active':'list-group-item'}
+				onClick={() => props.changeActive(task)}>
+				<div className="row">
+					<div className="col-sm-10">
+						<span>{props.task.task}</span>
+					</div>
+					<button className="col-sm-2 btn btn-danger pull-right"
+						onClick={(e) => props.deleteTask(task, e)}>削除</button>
+				</div>
+			</li>
+		);
+	}
 
 	function Tasks(props) {
 		const tasks = props.tasks.map(task => {
 			return (
-				<li>{task.task}</li>
+				<Task
+					task={task}
+					changeActive={props.changeActive}
+					deleteTask={props.deleteTask}
+				/>
 			);
 		});
 
@@ -21,13 +42,48 @@
 			this.state = {
 				tasks: tasks,
 			};
+			this.changeActive = this.changeActive.bind(this);
+			this.deleteTask = this.deleteTask.bind(this);
+		}
+
+		changeActive(task) {
+			const tasks = this.state.tasks.map(task => {
+				return {
+					id    : task.id,
+					task  : task.task,
+					active: task.active
+				}
+			});
+			const index = this.state.tasks.map(task => {
+				return task.id;
+			}).indexOf(task.id);
+
+			tasks[index].active = !tasks[index].active;
+
+			this.setState({
+				tasks: tasks,
+			});
+		}
+
+		deleteTask(task, e) {
+			const tasks = this.state.tasks.slice();
+			const index = this.state.tasks.indexOf(task);
+			// 該当タスクを削除
+			tasks.splice(index, 1);
+			// タスク一覧をセット
+			this.setState({
+				tasks: tasks
+			});
+			e.stopPropagation();
 		}
 
 		render() {
 			return (
-				<ul>
+				<ul className="list-group">
 					<Tasks
 						tasks={this.state.tasks}
+						changeActive={this.changeActive}
+						deleteTask={this.deleteTask}
 					/>
 				</ul>
 			);
